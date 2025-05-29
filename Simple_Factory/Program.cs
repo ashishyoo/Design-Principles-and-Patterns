f8dc3eb31d94571f33231fc90eb861615b254fa3
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-05-29 16:38:09 +0545

    feat: implement simple factory for coffee

---
+++ Simple_Factory/Program.cs
@@ -0,0 +1,59 @@
+﻿interface Coffee
+{
+    void Prepare();
+}
+
+class BasicCoffee : Coffee
+{
+    public void Prepare()
+    {
+        Console.WriteLine("Preparing Basic Coffee..");
+    }
+}
+
+class StandardCoffee : Coffee
+{
+    public void Prepare()
+    {
+        Console.WriteLine("Preparing Standard Coffee..");
+    }
+}
+
+class PremiumCoffee : Coffee
+{
+    public void Prepare()
+    {
+        Console.WriteLine("Preparing Premium Coffee..");
+    }
+}
+
+class CoffeeFactory
+{
+    public Coffee CreateCoffee(string type)
+    {
+        switch (type.ToLower())
+        {
+            case "basic":
+                return new BasicCoffee();
+            case "standard":
+                return new StandardCoffee();
+            case "premium":
+                return new PremiumCoffee();
+            default:
+                Console.WriteLine("Invalid coffee type");
+                return null;
+
+        }
+    }
+}
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        string type = "basic";
+        CoffeeFactory coffeeFactory = new CoffeeFactory();
+        var coffee = coffeeFactory.CreateCoffee(type);
+        coffee.Prepare(); // Output: Preparing Basic Coffee..
+    }
+}
