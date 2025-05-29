6125926c78dc41206049e7ea69cb3734ce104395
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-05-29 16:42:37 +0545

    feat: implement factory method

---
+++ Factory_Method/Program.cs
@@ -0,0 +1,106 @@
+﻿interface Coffee
+{
+    void Prepare();
+}
+
+class BasicCoffee : Coffee
+{
+    public void Prepare()
+    {
+        Console.WriteLine("Preparing Basic Coffee..");
+    }
+}
+
+class StandardCoffee : Coffee
+{
+    public void Prepare()
+    {
+        Console.WriteLine("Preparing Standard Coffee..");
+    }
+}
+
+class PremiumCoffee : Coffee
+{
+    public void Prepare()
+    {
+        Console.WriteLine("Preparing Premium Coffee..");
+    }
+}
+
+class BasicSoftCoffee : Coffee
+{
+    public void Prepare()
+    {
+        Console.WriteLine("Preparing Basic Soft Coffee..");
+    }
+}
+
+class StandardSoftCoffee : Coffee
+{
+    public void Prepare()
+    {
+        Console.WriteLine("Preparing Standard Soft Coffee..");
+    }
+}
+
+class PremiumSoftCoffee : Coffee
+{
+    public void Prepare()
+    {
+        Console.WriteLine("Preparing Premium Soft Coffee..");
+    }
+}
+
+interface CoffeeFactory
+{
+    Coffee CreateCoffee(string type);
+}
+
+class LocalCafe : CoffeeFactory
+{
+    public Coffee CreateCoffee(string type)
+    {
+        switch (type.ToLower())
+        {
+            case "basic":
+                return new BasicCoffee();
+            case "standard":
+                return new StandardCoffee();
+            case "premium":
+                return new PremiumCoffee();
+            default:
+                Console.WriteLine("Invalid coffee type");
+                return null;
+
+        }
+    }
+}
+class StarCafe : CoffeeFactory
+{
+    public Coffee CreateCoffee(string type)
+    {
+        switch (type.ToLower())
+        {
+            case "basic":
+                return new BasicSoftCoffee();
+            case "standard":
+                return new StandardSoftCoffee();
+            case "premium":
+                return new PremiumSoftCoffee();
+            default:
+                Console.WriteLine("Invalid coffee type");
+                return null;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        string type = "standard";
+        CoffeeFactory coffeeFactory = new LocalCafe();
+        var coffee = coffeeFactory.CreateCoffee(type);
+        coffee.Prepare(); // Output: Preparing Standard Coffee..
+    }
+}
