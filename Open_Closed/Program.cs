1afe44a5c1612993962dce99c8a7e8b266643ea9
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-05-29 14:51:14 +0545

    feat: implement open-closed principle

---
+++ Open_Closed/Program.cs
@@ -0,0 +1,36 @@
+﻿public interface IShape
+{
+    void Draw();
+}
+
+public class Circle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing Circle");
+    }
+}
+
+public class Square : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing Square");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IShape[] shapes = { new Circle(), new Square() };
+        foreach (var shape in shapes)
+        {
+            shape.Draw();
+            /*
+                Output: Drawing Circle
+                Output: Drawing Square
+            */
+        }
+    }
+}
