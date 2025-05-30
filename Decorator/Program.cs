6165b07a56a3903a689f30ae46f0643eb0457d1e
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-05-30 13:22:34 +0545

    refactor: include output in comment

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -6,17 +6,17 @@ class Program
     static void Main(String[] args)
     {
         ICoffee coffee = new SimpleCoffee();
-        Console.WriteLine(coffee.GetDescription());
+        Console.WriteLine(coffee.GetDescription()); // Output: Simple Coffee
         coffee = new SugarDecorator(coffee);
-        Console.WriteLine(coffee.GetDescription());
+        Console.WriteLine(coffee.GetDescription()); // Output: Simple Coffee with Sugar
         coffee = new MilkDecorator(coffee);
-        Console.WriteLine(coffee.GetDescription());
+        Console.WriteLine(coffee.GetDescription()); // Output: Simple Coffee with Sugar with Milk
 
         ICharacter mario = new Mario();
-        Console.WriteLine(mario.GetAbilities());
+        Console.WriteLine(mario.GetAbilities()); // Output: Mario
         mario = new GunPower(mario);
-        Console.WriteLine(mario.GetAbilities());
+        Console.WriteLine(mario.GetAbilities()); // Output: Mario with Gun power
         mario = new HeightPower(mario);
-        Console.WriteLine(mario.GetAbilities());
+        Console.WriteLine(mario.GetAbilities()); // Output: Mario with Gun power with Height power
     }
 }
